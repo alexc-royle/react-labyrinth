4194ba861238be635db5c5cb29573c43b9d96fab
Alex Royle <alex.royle@mydomain.com>
2017-08-26 15:43:07 +0100

    Updates

--- src/containers/BoardSquareList.js
+++
@@ -1,9 +0,0 @@
-/*
-import React from 'react'
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router';
-import * as actions from '../actions';
-import TodoList from '../components/TodoList';
-import { getVisibleTodos, getIsFetching, getErrorMessage } from '../reducers';
-import FetchError from '../components/FetchError';
-*/
